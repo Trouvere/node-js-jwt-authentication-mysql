96f305b77c68814f765a5fde82920c7913f5c58d
Aliaksei Mishyn <trouvere777@mail.ru>
2020-11-16 13:19:46 +0300

    Define Routes

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to THE BEST TEAM application." });
 });
 
+// routes
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
