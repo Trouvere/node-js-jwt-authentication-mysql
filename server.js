a76803141d4b0362a0d27be00126c23830c0a8a7
Aliaksei Mishyn <trouvere777@mail.ru>
2020-11-18 13:43:36 +0300

    Add setting lint-staged

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const db = require("./app/models");
+/* eslint-disable no-console */
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./app/models');
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000",
+const corsOptions = {
+  origin: 'http://localhost:3000'
 };
 
 app.use(cors(corsOptions));
@@ -18,12 +19,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "The best team!" });
+app.get('/', (req, res) => {
+  res.json({ message: 'The best team!' });
 });
 
-require("./app/routes/auth.routes")(app);
-require("./app/routes/user.routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -35,22 +36,23 @@ const Role = db.role;
 // db.sequelize.sync();
 
 db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and Resync Db");
+  console.log('Drop and Resync Db');
+  // eslint-disable-next-line no-use-before-define
   initial();
 });
 function initial() {
   Role.create({
     id: 1,
-    name: "user",
+    name: 'user'
   });
 
   Role.create({
     id: 2,
-    name: "moderator",
+    name: 'moderator'
   });
 
   Role.create({
     id: 3,
-    name: "admin",
+    name: 'admin'
   });
 }
